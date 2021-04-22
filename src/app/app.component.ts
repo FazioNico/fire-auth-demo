a32f2a875d8512373366574870e749477e459298
FazioNico <contact@nicolasfazio.ch>
2021-04-22 10:23:50 +0200

    upd: use basic auth system with firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'FireAuthDemo';
 
-  login() {}
-  logout() {}
+  constructor(public auth: AngularFireAuth) {}
+
+  login() {
+    this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+  logout() {
+    this.auth.signOut();
+  }
 }
